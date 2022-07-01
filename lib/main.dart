276f3b57a749055697469263699629241cbb14e0
Ramudgar <ramudgary52@gmail.com>
2022-07-01 11:55:22 +0545

    functional calculator is finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Calculator(),
     ),
   );
@@ -17,6 +18,27 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNumber = TextEditingController();
+  final TextEditingController _secondNumber = TextEditingController();
+
+  num result = 0;
+
+  add() {
+    setState(() {
+      result =
+          double.parse(_firstNumber.text) + double.parse(_secondNumber.text);
+    });
+  }
+
+  subtract() {
+    setState(() {
+      result =
+          double.parse(_firstNumber.text) - double.parse(_secondNumber.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,63 +48,72 @@ class _CalculatorState extends State<Calculator> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                  controller: _firstNumber,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      labelText: 'First Number',
+                      hintText: 'Enter First number',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ))),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: _secondNumber,
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
-                    labelText: 'First Number',
-                    hintText: 'Enter First number',
+                    labelText: 'Second Number',
+                    hintText: 'Enter Second number',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ))),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  labelText: 'Second Number',
-                  hintText: 'Enter Second number',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  )),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Add'),
+                    )),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Subtract'),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Card(
-              color: Colors.amberAccent,
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    add();
+                  },
+                  child: const Text('Add'),
                 ),
-                title: Text('Result: ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    subtract();
+                  },
+                  child: const Text('Subtract'),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Card(
+                color: Colors.amberAccent,
+                child: ListTile(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  title: Text('Result:$result ',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
